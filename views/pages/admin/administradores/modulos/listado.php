a31ed613c860378d1b352a320f7fd5e0ded75220
cesar david <3145434864c@gmail.com>
2025-10-22 09:19:47 -0500

    Empezamos a crear listas

---
+++ views/pages/admin/administradores/modulos/listado.php
@@ -0,0 +1,31 @@
+<div class="content">
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title text-white">
+                    <a href="" class="btn btn-principal py-2 px-3 btn-sm rounded-pill text-reset"><i class="fas fa-user-plus me-1"></i>Agregar Administrador</a>
+                </h3>
+            </div>
+        </div>
+
+            <div class="card-body">
+                <div class="table-resposive">
+                    <table class="table table-bordered table-striped aling-middle w-100" id="tablaAdmins">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Rol</th>
+                                <th>Ultimo Login</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
