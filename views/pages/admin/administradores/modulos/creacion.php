593cada4bf0f4118184f5f236824ff347608715e
cesar david <3145434864c@gmail.com>
2025-10-23 15:51:59 -0500

    d

--- views/pages/admin/administradores/modulos/creacion.php
+++ views/pages/admin/administradores/modulos/creacion.php
@@ -9,6 +9,9 @@
                             <div class="col-12 col-lg-6 col text-center text-lg-left">
                                 <h4 class="mt-3">Agregar Administrador</h4>
                             </div>
+                            <div class="col-12 col-lg-6 mt-2 d-none d-lg-block">
+                                <button type="submit" class="btn border-0 bg-principal float-rigth py-2 px-3 btn-sm rounded-pill">Registrar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
